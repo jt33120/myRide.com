87f78bd419f19db6181a37143335097adcb7fa6d
JulianTalou <julian.talou33@gmail.com>
2025-04-13 18:46:54 -0400

    Reciept update

--- pages/vehicleCard_page.js
+++ pages/vehicleCard_page.js
@@ -1522,27 +1522,6 @@ const handleDocumentUpload = async (documentType, file, expirationDate) => {
         }
       />
       <ToastContainer />
-            {/* Return Button */}
-            <button
-        onClick={() => router.push("/myVehicles_page")}
-        className="absolute top-4 left-4 bg-gray-200 p-2 rounded-full shadow-md hover:bg-gray-300 z-50" // Increased z-index
-        title="Go Back"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="h-6 w-6 text-gray-600"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5 8.25 12l7.5-7.5"
-          />
-        </svg>
-      </button>
 
       <ImageCarousel imageUrls={imageUrls} />
 
